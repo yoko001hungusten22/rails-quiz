9ecbd3b4e224540ef8b7c4f6e562b91fb61ce907
EC2 Default User <ec2-user@ip-172-31-4-177.us-east-2.compute.internal>
2023-05-22 05:17:24 +0000

    プロフィール編集機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :name, presence: true, length: { maximum: 50 }
+  
+  def update_without_current_password(params, *options)
+    params.delete(:password)
+    params.delete(:password_confirmation)
+  
+    update(params, *options)
+  end
 end
